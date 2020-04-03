91c43a9dc822da30cf3cd2908891edddcea482f2
Matías Lang <matiasl@infobytesec.com>
2020-04-03 16:10:46 -0300

    Add apispec python package
    It is based on the one in commit
    f6702bc836b60892016be09c4bbd4e7d5ebde5c6, but disabled the tests to
    avoid adding prance too. Also add pyyaml as a runtime dependency so it
    can be used with apispec-webframework

---
+++ pkgs/development/python-modules/apispec/default.nix
@@ -0,0 +1,25 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pyyaml
+}:
+
+buildPythonPackage rec {
+  pname = "apispec";
+  version = "3.1.1";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "24b8490c22310b0779a058ccb24ec2fef33d571bb5aba1f525ab5963b0eabcdd";
+  };
+
+  propagatedBuildInputs = [ pyyaml ];
+  doCheck = false;
+
+  meta = with lib; {
+    description = "A pluggable API specification generator. Currently supports the OpenAPI Specification (f.k.a. the Swagger specification";
+    homepage = https://github.com/marshmallow-code/apispec;
+    license = licenses.mit;
+    maintainers = [ maintainers.costrouc ];
+  };
+}
