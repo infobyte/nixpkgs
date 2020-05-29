19efb96bab2efa464328af434cecead62f6cfd5e
Matías Lang <matiasl@infobytesec.com>
2020-05-29 16:56:50 -0300

    python3Packages.pygobject: fix build
    It was missing meson dependency

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5740,7 +5740,7 @@ in {
   };
 
   pygobject = callPackage ../development/python-modules/pygobject/3.nix {
-    inherit (pkgs) pkgconfig;
+    inherit (pkgs) meson pkgconfig;
   };
 
   pygogo = callPackage ../development/python-modules/pygogo { };
