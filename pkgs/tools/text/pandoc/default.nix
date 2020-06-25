d1f12b4d7e20ba59fd97b61ba193add8f7d8402e
Matías Lang <matiasl@infobytesec.com>
2020-06-25 17:27:49 -0300

    pandoc: Support x86_64-darwin

--- pkgs/tools/text/pandoc/default.nix
+++ pkgs/tools/text/pandoc/default.nix
@@ -1,13 +1,23 @@
-{ stdenv }:
+{ stdenv, fetchzip }:
 
-stdenv.mkDerivation rec {
-  name = "pandoc-${version}";
+let
   version = "2.9.2.1";
-  src = builtins.fetchurl {
-    url =
-      "https://github.com/jgm/pandoc/releases/download/${version}/pandoc-${version}-linux-amd64.tar.gz";
-    sha256 = "1y0c9cdhk1w7l3qx3l265dzgjcb673qqhmxsnk0lhz9bpn0sjqav";
+  sources = {
+    x86_64-linux = builtins.fetchurl {
+      url =
+        "https://github.com/jgm/pandoc/releases/download/${version}/pandoc-${version}-linux-amd64.tar.gz";
+      sha256 = "1y0c9cdhk1w7l3qx3l265dzgjcb673qqhmxsnk0lhz9bpn0sjqav";
+    };
+    x86_64-darwin = fetchzip {
+      url =
+        "https://github.com/jgm/pandoc/releases/download/${version}/pandoc-${version}-macOS.zip";
+      sha256 = "09idjjbz0dhiiydhwn9c87arf8l0c19rjypqjgwvyhn612l516j8";
+    };
   };
+in stdenv.mkDerivation rec {
+  name = "pandoc-${version}";
+  inherit version;
+  src = sources.${stdenv.system};
   installPhase = ''
     mkdir -p $out
     cp -r bin/ share/ $out/
