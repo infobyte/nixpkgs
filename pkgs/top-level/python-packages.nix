ee27c439178fe7a30f5edcbe2f08f381ba30493c
Matías Lang <matiasl@infobytesec.com>
2020-08-12 18:46:09 -0300

    pythonPackages.cairocffi: Use cairo without openGL
    OpenGL incrases the closure size of weasyprint significantly.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1015,7 +1015,11 @@ in {
   });
 
   cairocffi = if isPy3k then
-    callPackage ../development/python-modules/cairocffi { }
+    callPackage ../development/python-modules/cairocffi {
+      cairo = pkgs.cairo.override {
+        glSupport = false;
+      };
+    }
   else
     callPackage ../development/python-modules/cairocffi/0_9.nix { };
 
